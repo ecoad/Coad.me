fb226c1d540a17e35673aeb77e35815a7bbe5283
ecoad <elliotcoad@gmail.com>
2012-06-19 13:06:47 +0100

    Project setup

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+app.version = '0.1.1';
+var versionator = require('versionator').create(app.version);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(versionator.middleware);
+  app.use(express.static(__dirname + '/public', { maxAge: 2592000000 }));
+});
+
+app.helpers({
+  versionPath: versionator.versionPath
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
