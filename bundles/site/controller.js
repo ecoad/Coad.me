a0c600e894c9f175217ddc2a88ed79f88fdb6f3a
ecoad <elliotcoad@gmail.com>
2012-06-22 22:49:39 +0100

    Moving cycle in to Site bundle

--- bundles/site/controller.js
+++ bundles/site/controller.js
@@ -2,4 +2,16 @@ module.exports.createRoutes = function(serviceLocator, bundleViewPath) {
   serviceLocator.app.get('/', function(req, res) {
     res.render(bundleViewPath + '/home');
   });
+
+  serviceLocator.app.get('/cycle', function(req, res) {
+    var now = new Date()
+      , then = new Date('2012-07-31')
+      , days
+      , weeks;
+
+    days = Math.floor(((((then.getTime() - now.getTime()) / 1000) / 60) / 60) / 24);
+    weeks = Math.ceil(days / 7);
+
+    res.render(bundleViewPath + '/cycle', {days: days, weeks: weeks});
+  });
 };
